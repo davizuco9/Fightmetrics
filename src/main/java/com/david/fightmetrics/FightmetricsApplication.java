82352d18f85d3fd688591aa79bde8b4decdbcf05
davizuco9 <davidperezdaw9@gmail.com>
2026-07-30 18:22:19 +0200

    Initialize FightMetrics project

---
+++ src/main/java/com/david/fightmetrics/FightmetricsApplication.java
@@ -0,0 +1,14 @@
+package com.david.fightmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightmetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightmetricsApplication.class, args);
+	}
+
+}
+
